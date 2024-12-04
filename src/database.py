b34a646a0bca2ad9e7d3ec2316f39f6e575d4ea9
hrauds <hanna.raudsepp@gmail.com>
2024-12-04 15:03:18 +0200

    frame_values salvestatakse ainult väärtuste listina recording kollektsiooni, ilma ajatempliteta, et vähendada salvestusruumi kasutust. Lisatud mean objekt recording, word ja phoneme kollektsioonidesse, mis sisaldab tunnuste nimesid koos nende keskmiste väärtustega. frame_values serialiseeritakse nüüd pickle abil ja salvestatakse binaarobjektidena.

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+import pickle
 from pymongo import MongoClient, errors
 from config import MONGO_URI
 import logging
@@ -78,10 +79,7 @@ class Database:
         """
         try:
             exists = self.recordings_col.find_one({"recording_id": recording_id}, {"_id": 1})
-            if exists:
-                return True
-            else:
-                return False
+            return bool(exists)
         except errors.PyMongoError as e:
             logging.error(f"Failed to check existence of recording with ID '{recording_id}': {e}")
             return False
@@ -115,36 +113,64 @@ class Database:
         """
         features = {}
         try:
-            if analysis_level == 'recording':
-                collection = self.recordings_col
-            elif analysis_level == 'word':
-                collection = self.words_col
-            elif analysis_level == 'phoneme':
-                collection = self.phonemes_col
-            else:
+            if analysis_level not in ['recording', 'word', 'phoneme']:
                 logging.error(f"Invalid analysis level: {analysis_level}")
                 return features
 
-            # Get features for each recording id
+            collection = {
+                'recording': self.recordings_col,
+                'word': self.words_col,
+                'phoneme': self.phonemes_col
+            }.get(analysis_level)
+
             for recording_id in recording_ids:
                 query = {"recording_id": recording_id}
                 cursor = collection.find(query)
                 features_list = list(cursor)
 
                 if features_list:
-                    formatted_features = [
-                        {
+                    formatted_features = []
+
+                    # Get serialized frame data from the recordings collection
+                    recording_doc = self.recordings_col.find_one({"recording_id": recording_id})
+                    if not recording_doc:
+                        logging.warning(f"Recording data not found for ID '{recording_id}'")
+                        continue
+
+                    full_frame_values = self.deserialize_frame_values(recording_doc["features"]["frame_values"])
+
+                    for feature in features_list:
+                        start, end = feature.get("start"), feature.get("end")
+
+                        # Slice frame values based on the interval
+                        sliced_frame_values = [
+                            (float(timestamp), [float(value) for value in values])
+                            for timestamp, values in zip(full_frame_values["timestamps"], full_frame_values["values"])
+                            if start <= timestamp <= end
+                        ]
+
+                        # Downsample frame values for recording and word levels
+                        if analysis_level in ['recording', 'word']:
+                            if analysis_level == 'recording':
+                                step = 10
+                            else:
+                                step = 2
+                            sliced_frame_values = sliced_frame_values[::step]
+
+                        formatted_features.append({
+                            "_id": str(feature.get("_id")),
                             "text": feature.get("text"),
-                            "start": feature.get("start"),
-                            "end": feature.get("end"),
-                            "mean": feature.get("features", {}).get("mean"),
-                            "frame_values": feature.get("features", {}).get("frame_values"),
-                        }
-                        for feature in features_list
-                    ]
+                            "start": start,
+                            "end": end,
+                            "mean": {
+                                key: float(value) for key, value in feature.get("features", {}).get("mean", {}).items()
+                            },
+                            "frame_values": sliced_frame_values
+                        })
+
                     features[recording_id] = formatted_features
                     logging.info(
-                        f"Fetched {len(features_list)} frame values for recording '{recording_id}' at level '{analysis_level}'.")
+                        f"Fetched {len(formatted_features)} features for recording '{recording_id}' at level '{analysis_level}'.")
                 else:
                     logging.warning(f"No features found for recording '{recording_id}' at level '{analysis_level}'.")
 
@@ -152,6 +178,23 @@ class Database:
         except Exception as e:
             logging.error(f"Error fetching features for recordings {recording_ids} at level {analysis_level}: {e}")
             return {}
+
+    @staticmethod
+    def deserialize_frame_values(frame_values):
+        """
+        Deserialize the frame values stored in the database.
+
+        Args:
+            frame_values (bytes): Serialized frame values.
+
+        Returns:
+            dict: Deserialized frame values with timestamps and feature values.
+        """
+        try:
+            return pickle.loads(frame_values)
+        except Exception as e:
+            logging.error(f"Failed to deserialize frame values: {e}")
+            return None
 
     def close_connection(self):
         """
